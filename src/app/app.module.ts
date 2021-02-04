582b22de5e63e5fefcc3845692a4216f064f09be
resacahya <resa.cahyasaputra@aai.astra.co.id>
2021-02-05 01:32:43 +0700

    first init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,28 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://localhost:9002',
+          prefix: '/astramarketplacewebservices/v2/'
+        }
+      },
+      context: {
+        currency: ['IDR'],
+        language: ['in'],
+        baseSite:['marketplace-spa']
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '2.1'
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
